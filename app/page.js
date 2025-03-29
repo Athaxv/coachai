55ac333b557217bb219893d82778043f6765bdd5
AtharvGaur <atharava112@gmail.com>
2025-03-29 12:18:08 +0530

    Add user authentication middleware and integrate UserButton component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { UserButton } from "@stackframe/stack";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Hola, Amigos</h2>
+      <UserButton/>
     </div>
   );
 }
