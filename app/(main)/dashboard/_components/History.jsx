8de54dec2434576de5cea5c5404a920577e6c4e3
AtharvGaur <atharava112@gmail.com>
2025-03-30 21:25:09 +0530

    Add History component to display user's previous lectures

---
+++ app/(main)/dashboard/_components/History.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function History() {
+  return (
+    <div>
+        <h2 className='font-bold text-xl'>Your Previous Lecture</h2>
+        <p className='text-gray-400'>You don't have any previous lecture</p>
+    </div>
+  )
+}
+
+export default History
